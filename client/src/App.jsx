d324992f9e207be372b9c0a8bb521617ac2412e4
Akhil Modi <akhilmodi29@gmail.com>
2026-07-18 13:40:29 +0530

    Update API endpoint to production URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3001/analyze', {
+      const response = await fetch('https://codesentinel-api-d660.onrender.com/analyze',{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ issueUrl })
